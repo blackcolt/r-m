82c05ecc79599839e0c2d5ed35dc0aa7ad766ebf
idan.magled <idan.magled@vesttoo.com>
2024-03-16 22:07:51 +0200

    Another lesson

--- src/components/Character/Character.tsx
+++ src/components/Character/Character.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 interface CharacterProps {
   name: string;
@@ -14,7 +15,7 @@ export const Character: FC<CharacterProps> = (props) => {
   return (
     <div style={{ display: "inline-grid", margin: "10px" }}>
       <Link to={`/r-m/characters/${id}`}>
-        <LazyLoadImage onLoad={() => console.log(1)} src={image} />
+        <LazyLoadImage effect="blur" src={image} />
         <div className="name">{name}</div>
         <div className="specie">{species}</div>
       </Link>
