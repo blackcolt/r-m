3c813a7aa235dc05e9e4f1ce2347f174e4ab3ada
idan.magled <idan.magled@vesttoo.com>
2024-03-16 21:32:47 +0200

    Another lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePageTsx from "./pages/HomePage/HomePage";
-// import DetailPage from "./pages/DetailPage/DetailPage";
-// import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-<script>console.log(111)</script>;
-// const queryClient = new QueryClient({
-//   defaultOptions: {
-//     queries: {
-//       staleTime: Infinity,
-//     },
-//   },
-// });
+import DetailPage from "./pages/DetailPage/DetailPage";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+    },
+  },
+});
 
 function App() {
-  return <HomePageTsx />;
+  return (
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/r-m" element={<HomePageTsx />}></Route>
+          <Route path="/characters/:id" element={<DetailPage />}></Route>
+        </Routes>
+      </QueryClientProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
