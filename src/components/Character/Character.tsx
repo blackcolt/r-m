5f8cf3123ee6be73523bccd543525c47bfde8e4d
idan.magled <idan.magled@vesttoo.com>
2024-03-16 21:54:14 +0200

    Another lesson

--- src/components/Character/Character.tsx
+++ src/components/Character/Character.tsx
@@ -12,7 +12,7 @@ export const Character: FC<CharacterProps> = (props) => {
   const { id, name, image, species } = props;
   return (
     <div style={{ display: "inline-grid", margin: "10px" }}>
-      <Link to={`/characters/${id}`}>
+      <Link to={`/r-m/characters/${id}`}>
         <img src={image} />
         <div className="name">{name}</div>
         <div className="specie">{species}</div>
