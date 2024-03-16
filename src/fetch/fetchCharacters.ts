e66c3e8c1e6b96a3d9c8eff4f0f2af82c02549c0
idan.magled <idan.magled@vesttoo.com>
2024-03-16 21:06:50 +0200

    Another lesson

--- src/fetch/fetchCharacters.ts
+++ src/fetch/fetchCharacters.ts
@@ -6,7 +6,7 @@ interface fetchCharactersProps {
 export const fetchCharacters = async (props?: fetchCharactersProps) => {
   const { id, page = 1 } = props || {};
   const response = await fetch(
-    `${import.meta.env.VITE_RICK_AND_MORTY_API_BASE_URL}character/${id ?? ""}?page=${page}`
+    `https://rickandmortyapi.com/api/character/${id ?? ""}?page=${page}`
   );
   if (!response.ok) throw "error fetching character";
   const responseData = await response.json();
